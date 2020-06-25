306bef2b9fa24f9ca21c631cec9069a098542534
Erik <happy@MacBook-Pro.local>
2020-06-25 19:28:01 +0800

    Lesson 4

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/projects', 'ProjectsController@index');
-
+Route::get('/projects/{project}', 'ProjectsController@show');
 Route::post('/projects', 'ProjectsController@store');
