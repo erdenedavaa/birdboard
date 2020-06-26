134d624af3bf27fe9f6b18ff4665dec5c40b50b9
Erik <happy@MacBook-Pro.local>
2020-06-26 17:52:39 +0800

    Lesson 6

--- app/Project.php
+++ app/Project.php
@@ -12,4 +12,10 @@ class Project extends Model
     {
         return "/projects/{$this->id}";
     }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,12 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/projects', 'ProjectsController@index');
-Route::get('/projects/{project}', 'ProjectsController@show');
-Route::post('/projects', 'ProjectsController@store')->middleware('auth');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/projects', 'ProjectsController@index');
+    Route::get('/projects/{project}', 'ProjectsController@show');
+    Route::post('/projects', 'ProjectsController@store');
+
+    Route::get('/home', 'HomeController@index')->name('home');
+});
+
