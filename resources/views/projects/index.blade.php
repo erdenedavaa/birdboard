9d20c47614c6c927b44dc58d76d3c6e9878f25cb
Erik <happy@MacBook-Pro.local>
2020-06-30 11:27:46 +0800

    Lesson 10

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,18 +2,28 @@
 
 @section('content')
 
-    <div class="flex items-center mb-3">
-        <a href="/projects/create">New Project</a>
-    </div>
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-center w-full">
+            <h2 class="text-sm font-normal text-gray-600">My Projects</h2>
+            <a href="/projects/create" class="button">New Project</a>
+        </div>
+    </header>
 
-    <ul>
+    <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse($projects as $project)
-            <li>
-                <a href="{{ $project->path() }}">{{ $project->title }}</a>
-            </li>
+            <div class="lg:w-1/3 px-3 pb-6">
+                <div class="bg-white rounded-lg shadow p-5" style="height: 200px">
+                    <h3 class="font-normal text-xl py-4 -ml-5 border-l-4 border-blue-400 pl-4 mb-3">
+                        <a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
+                    </h3>
 
+{{--                    <div class="text-gray-600">{{ Str::limit($project->description, 100) }}</div>--}}
+                    <div class="text-gray-600">{{ str_limit($project->description, 100) }}</div>
+                </div>
+            </div>
         @empty
-            <li>No projects yet.</li>
+            <div>No projects yet.</div>
         @endforelse
-    </ul>
+    </main>
+
 @endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,15 @@
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
+// require('laravel-mix-postcss-config');
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
+    .sass('./resources/sass/app.scss', './public/css')
     .options({
         processCssUrls: false,
         postCss: [tailwindcss('./tailwind.config.js')],
     });
+    // .postCssConfig();
+
+// mix.js('resources/js/app.js', 'public/js')
+//     .postCss('resources/css/app.css', 'public/css')
+//     .postCssConfig();
