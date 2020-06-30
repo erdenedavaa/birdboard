33638ac2af1e82a2b24f59fdf6f1a72f666601b4
Erik <happy@MacBook-Pro.local>
2020-06-30 22:34:43 +0800

    Lesson 11

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <header class="flex items-center mb-3 py-4">
-        <div class="flex justify-between items-center w-full">
+        <div class="flex justify-between items-end w-full">
             <h2 class="text-sm font-normal text-gray-600">My Projects</h2>
             <a href="/projects/create" class="button">New Project</a>
         </div>
@@ -12,14 +12,7 @@
     <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse($projects as $project)
             <div class="lg:w-1/3 px-3 pb-6">
-                <div class="bg-white rounded-lg shadow p-5" style="height: 200px">
-                    <h3 class="font-normal text-xl py-4 -ml-5 border-l-4 border-blue-400 pl-4 mb-3">
-                        <a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
-                    </h3>
-
-{{--                    <div class="text-gray-600">{{ Str::limit($project->description, 100) }}</div>--}}
-                    <div class="text-gray-600">{{ str_limit($project->description, 100) }}</div>
-                </div>
+                @include('projects.card')
             </div>
         @empty
             <div>No projects yet.</div>
