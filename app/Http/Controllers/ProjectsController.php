719b9fbec9ed676908f403da35e80013a0fb5a09
Erik <happy@MacBook-Pro.local>
2020-07-06 22:51:10 +0800

    Lesson 16

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -1,53 +1,69 @@
 <?php
 
-namespace App\Http\Controllers;
+    namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Project;
+    use Illuminate\Http\Request;
+    use App\Project;
 
-class ProjectsController extends Controller
-{
-    public function index()
+    class ProjectsController extends Controller
     {
-        // Jeffray ene urd argad durgui, iluu deer arga ni model deer suulgaj uguh. User model haraarai.
-//        $projects = auth()->user()->projects()->orderBy('updated_at', 'desc')->get();
-        $projects = auth()->user()->projects;
+        public function index()
+        {
+            // Jeffray ene urd argad durgui, iluu deer arga ni model deer suulgaj uguh. User model haraarai.
+            //        $projects = auth()->user()->projects()->orderBy('updated_at', 'desc')->get();
+            $projects = auth()->user()->projects;
 
-        return view('projects.index', compact('projects'));
-    }
-
-    public function show(Project $project)
-    {
-//        $project = Project::findOrFail(request('project'));
-
-        if (auth()->user()->isNot($project->owner)) {
-            abort(403);
+            return view( 'projects.index', compact( 'projects' ) );
         }
 
-        return view('projects.show', compact('project'));
+        public function show(Project $project)
+        {
+            //        $project = Project::findOrFail(request('project'));
+
+            $this->authorize('update', $project);
+
+            return view( 'projects.show', compact( 'project' ) );
+        }
+
+        public function create()
+        {
+            return view( 'projects.create' );
+        }
+
+        public function store()
+        {
+            $attributes = request()->validate( ['title' => 'required', 'description' => 'required', 'notes' => 'min:3'] );
+
+            //        dd($attributes); // validate hiigeegui form automataar gardaggui gedgiig haruulah zorilgoor bichiv
+
+            //        $attributes['owner_id'] = auth()->id();
+
+            // owner_id create automatically
+            $project = auth()->user()->projects()->create( $attributes );
+
+
+            //        Project::create($attributes);  deer oruulsan bolhoor hasagdsan
+
+            return redirect( $project->path() );
+        }
+
+        public function update(Project $project)
+        {
+//            if (auth()->user()->isNot( $project->owner )) {
+//                abort( 403 );
+//            }
+            // deerhiig daraah baidlaar POLICTY hiine.
+            $this->authorize('update', $project);
+
+            // Ehnii huvilbar
+            //            $project -> update(
+            //                ['notes' => request( 'notes' )]
+            //            );
+
+            // Daraagiin huvilbar
+            $project->update( request( ['notes'] ) );
+
+            return redirect( $project->path() );
+        }
+
     }
-
-    public function create()
-    {
-        return view('projects.create');
-    }
-
-    public function store()
-    {
-        $attributes = request()->validate([
-            'title' => 'required',
-            'description' => 'required'
-        ]);
-
-//        $attributes['owner_id'] = auth()->id();
-
-        // owner_id create automatically
-        $project = auth()->user()->projects()->create($attributes);
-
-
-//        Project::create($attributes);  deer oruulsan bolhoor hasagdsan
-
-        return redirect($project->path());
-    }
-
-}
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -41,8 +41,21 @@
 
                 <div>
                     <h2 class="text-lg font-normal text-gray-600 mb-3">General Notes</h2>
+
                     <!-- general notes -->
-                    <textarea class="card w-full" style="min-height: 200px">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, facere.</textarea>
+                    <form method="POST" action="{{ $project->path() }}">
+                        @csrf
+                        @method('PATCH')
+
+                        <textarea
+                            name="notes"
+                            class="card w-full mb-4"
+                            style="min-height: 200px"
+                            placeholder="Anything special that you want to make a note of?"
+                        >{{ $project->notes }}</textarea>
+
+                        <button type="submit" class="button">Save</button>
+                    </form>
                 </div>
             </div>
             <div class="lg:w-1/4 px-3 lg:py-10">
